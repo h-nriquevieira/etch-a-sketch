5e5314e0a8479d1cbcda76b7fa66084b53bacc9c
h-nriquevieira <h.nriquevieira@gmail.com>
2022-02-14 09:19:51 -0300

    Make app functional

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
 const gridContainer = document.querySelector('.grid-container');
 const gridHeight = gridContainer.offsetHeight;
+let squares;
+const reset = document.querySelector('#reset');
+const gridCreate = document.querySelector('#create-grid');
 
 
 function createRows(gridSize) {
   for (let i = 0; i < gridSize; i++) {
     const row = document.createElement('div');
-    row.style.cssText = `display: flex; flex: 1; border: 1px solid red;`;
+    row.style.cssText = `display: flex; flex: 1;`;
     row.id = `row-${i+1}`;
     const rowIdentifier = i+1;
     createSquares(gridSize, row, rowIdentifier);
@@ -16,18 +19,41 @@ function createRows(gridSize) {
 function createSquares(numOfSquares, row, rowId) {
   for (let i = 0; i < numOfSquares; i++) {
     const square = document.createElement('div');
-    square.style.cssText = 'flex: 1; border: 1px solid purple;';
+    square.style.cssText = 'flex: 1; border: 1px solid lightgray;';
     square.id = `square-${rowId}-${i+1}`;
     row.appendChild(square);
   }
 }
 
+function resetGrid() {
+  let child = gridContainer.lastElementChild;
+  while (child) {
+    gridContainer.removeChild(child);
+    child = gridContainer.lastElementChild;
+  }
+}
 
-/* 
-- Get grid size and store it in a variable
-- Divide container height by grid size to get square side
-- Create a row container
-- Create gridSize number of squares with squareSide and add them to row container
-- Create gridSize number of rowContainers
-- Add all of them to grid container
-*/
+function addEvents() {
+  squares = document.querySelectorAll(`[id*="square"]`);
+  squares.forEach(square => square.addEventListener('mouseover', changeBackground));
+}
+
+function createGrid(gridSize) {
+  resetGrid();
+  createRows(gridSize);
+  addEvents();
+}
+
+function changeBackground() {
+  this.style.cssText += 'background-color: black;';  
+}
+
+function resetButton() {
+  resetGrid();
+  const gridSize = prompt('Enter a grid size (up to 100):');
+  if (gridSize > 100) resetButton();
+  createGrid(gridSize);
+}
+
+reset.addEventListener('click', resetButton);
+gridCreate.addEventListener('click', createGrid);
